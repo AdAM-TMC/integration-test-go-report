6548e6ac7e3319bd13f03b7e3a98f46d6cdc2069
mcerezuela <marcus.cerezuela@tmc.nl>
2024-10-25 12:51:09 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs');
+const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
+      on('task', {
+        appendToFile({ fileName, data }) {
+          fs.appendFileSync(path.join(__dirname, fileName), data + '\n');
+          return null;
+        },
+      });
     },
   },
 });
